5c7467c3a1c115852791633487460d3e4846a2d2
sufffix <62039916+sufffix@users.noreply.github.com>
2021-09-08 14:25:47 -0600

    finished rgb colour javascript

---
+++ cs10/RGB Colour Code Along/script.js
@@ -0,0 +1,20 @@
+// RGB Colours
+
+// Event Listener
+document.getElementById("preview-btn").addEventListener("click", rgbPreview);
+
+// Event Function
+function rgbPreview() {
+    // Input: get rgb values
+    let rValue = document.getElementById("redIn").value;
+    let gValue = document.getElementById("greenIn").value;
+    let bValue = document.getElementById("blueIn").value;
+
+    // Process: build rgb string rgb(_, _, _)
+    let rgbString = "rgb(" + rValue + ", " + gValue + ", " + bValue + ")";
+
+    // Output: display rgb string and update color preview
+    document.getElementById("rgbStrOut").innerHTML = rgbString;
+    document.getElementById("display").style.background = rgbString;
+
+}
