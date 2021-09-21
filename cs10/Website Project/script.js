d2f57cc156a9c6b09cfe5f88a57fd7b18612831a
sufffix <62039916+sufffix@users.noreply.github.com>
2021-09-21 14:07:21 -0600

    finished quiz + about pages

--- cs10/Website Project/script.js
+++ cs10/Website Project/script.js
@@ -12,66 +12,61 @@ let q3StatusEl = document.getElementById("q3Status");
 let q4StatusEl = document.getElementById("q4Status");
 
 let submitBtnEl = document.getElementById("submitBtn");
+let resultParEl = document.getElementById("resultPar");
 
 //  event listener
 submitBtnEl.addEventListener("click", submitQuiz);
 
 // event function
 function submitQuiz() {
-    // declare question variables
-    let q1Correct = false;
-    let q2Correct = false;
-    let q3Correct = false;
-    let q4Correct = false;
+    let numCorrect = 0;
 
     // check answers
     if (q1InEl.value.toLowerCase() === "zenimax" || q1InEl.value.toLowerCase() === "zenimax online" || q1InEl.value.toLowerCase() === "zenimax online studios") {
-        q1Correct = true;
-    } 
-
-    if (q2InEl.value.toLowerCase() === "aldmeri dominion" || q2InEl.value.toLowerCase() === "the dominion" || q2InEl.value.toLowerCase() === "the aldmeri dominion") {
-        q2Correct = true;
-    }
-
-    if (q3InEl.value.toLowerCase() === "morrowind") {
-        q3Correct = true;
-    }
-
-    if (q4InEl.value.toLowerCase() === "eso plus") {
-        q4Correct = true;
-    }
-
-    // change status text
-    if (q1Correct === true) {
         q1StatusEl.innerHTML = "Correct";
         q1StatusEl.style.color = "lime";
+        numCorrect += 1;
     } else {
         q1StatusEl.innerHTML = "Correct Answer: Zenimax Online Studios";
         q1StatusEl.style.color = "red";
     }
 
-    if (q2Correct === true) {
+    if (q2InEl.value.toLowerCase() === "aldmeri dominion" || q2InEl.value.toLowerCase() === "the dominion" || q2InEl.value.toLowerCase() === "the aldmeri dominion") {
         q2StatusEl.innerHTML = "Correct";
         q2StatusEl.style.color = "lime";
+        numCorrect += 1;
     } else {
         q2StatusEl.innerHTML = "Correct Answer: Aldmeri Dominion";
         q2StatusEl.style.color = "red";
     }
 
-    if (q3Correct === true) {
+    if (q3InEl.value.toLowerCase() === "morrowind") {
         q3StatusEl.innerHTML = "Correct";
         q3StatusEl.style.color = "lime";
+        numCorrect += 1;
     } else {
         q3StatusEl.innerHTML = "Correct Answer: Morrowind";
         q3StatusEl.style.color = "red";
     }
 
-    if (q4Correct === true) {
+    if (q4InEl.value.toLowerCase() === "eso plus") {
         q4StatusEl.innerHTML = "Correct";
         q4StatusEl.style.color = "lime";
+        numCorrect += 1;
     } else {
         q4StatusEl.innerHTML = "Correct Answer: ESO Plus";
         q4StatusEl.style.color = "red";
+    }
+        
+    if (numCorrect > 2) {
+        resultParEl.style.color = "lime";
+        resultParEl.innerHTML = "You got " + numCorrect + " correct. Good job!";
+    } else if (numCorrect < 2) {
+        resultParEl.style.color = "red";
+        resultParEl.innerHTML = "You got " + numCorrect + " correct. Good try!"
+    } else {
+        resultParEl.style.color = "yellow";
+        resultParEl.innerHTML = "You got " + numCorrect + " correct. Good try!"
     }
 
 }
